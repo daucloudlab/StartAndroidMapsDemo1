e50f2e7010cd764ea7c445b05b40ad5175c181cb
Daulet Baimbetov <daucloudlab@gmail.com>
2015-06-06 12:12:58 +0600

    Simple Maps Demo project

---
+++ app/src/main/java/kz/abcsoft/android/develop/maps/MainActivity.java
@@ -0,0 +1,65 @@
+package kz.abcsoft.android.develop.maps;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    SupportMapFragment mapFragment ;
+    GoogleMap map ;
+
+    final String TAG = "myLogs" ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map) ;
+        map = mapFragment.getMap() ;
+
+        if(map == null){
+            finish() ;
+            return ;
+        }
+        init() ;
+    }
+
+    private void init(){
+
+    }
+
+    public void onClickTest(View v){
+        map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
